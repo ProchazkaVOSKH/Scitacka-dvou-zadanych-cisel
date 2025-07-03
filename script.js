368171cc89bf6bca3e11b6ce555c5b63c87379a0
Martin Procházka <prochazka@voskh.cz>
2025-07-03 07:47:32 +0200

    Publikováno řešení

---
+++ script.js
@@ -0,0 +1,35 @@
+// Získání referencí na HTML prvky podle jejich ID.
+const tbxCislo1 = document.getElementById("operand1");
+const tbxCislo2 = document.getElementById("operand2");
+const tbxVysledek = document.getElementById("result");
+const btnSoucet = document.getElementById("soucet");
+const obrazek = document.getElementById("o1");
+const obsahStranky = document.body;
+
+// Nastavení výchozí barvy pozadí stránky.
+obsahStranky.style.backgroundColor = "#ADD8E6";
+
+// Funkce pro sečtení dvou čísel zadaných uživatelem.
+function secti() {
+    // Převod hodnot z textových polí na čísla.
+    let cislo1 = Number(tbxCislo1.value);
+    let cislo2 = Number(tbxCislo2.value);
+    // Výpočet součtu.
+    let vysledek = cislo1 + cislo2;
+    // Zobrazení výsledku v textovém poli.
+    tbxVysledek.value = vysledek;
+
+    // Podmínka pro změnu barvy pozadí a obrázku podle výsledku.
+    if (vysledek >= 0) {
+        obsahStranky.style.backgroundColor = "green"; // Zelené pozadí pro kladný výsledek.
+        o1.src = "dobre.jpg"; // Obrázek pro kladný výsledek.
+    } else {
+        obsahStranky.style.backgroundColor = "red"; // Červené pozadí pro záporný výsledek.
+        o1.src = "chyba.jpg"; // Obrázek pro záporný výsledek.
+    }
+}
+
+// Přiřazení funkce secti k události kliknutí na tlačítko.
+btnSoucet.onclick = secti;
+// Přiřazení funkce secti k události kliknutí na textové pole výsledku.
+tbxVysledek.onclick = secti;
